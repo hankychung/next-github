be4d9543d54a07ce397c41ed7877dd6843f97add
Han <hanky_chung@hotmail.com>
2020-02-01 22:36:06 +0800

    add eslint

--- redis/index.js
+++ redis/index.js
@@ -2,9 +2,9 @@ const Redis = require('ioredis')
 
 const redis = new Redis({
   port: 6379, // Redis port
-  host: "awesomehan.com", // Redis host
+  host: 'awesomehan.com', // Redis host
   family: 4, // 4 (IPv4) or 6 (IPv6)
-  password: "han",
-});
+  password: 'han'
+})
 
 redis.set('other', 'hello world')
